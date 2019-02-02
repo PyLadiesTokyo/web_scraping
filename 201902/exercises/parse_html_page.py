e479735bfe2892fdc8b0cac19615ee9041a650e4
okusama27 <kaz@iron-hot.com>
2019-02-02 23:58:48 +0900

    Change exercises code

--- 201902/exercises/parse_html_page.py
+++ 201902/exercises/parse_html_page.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup  # ①
 
 html = open('pyladies-locations.html').read()  # ②
 soup = BeautifulSoup(html, 'html.parser')  # ③
+
+# 地域のPyLaidesの特定のタグを検索
 records = soup.find_all('div', class_="chapter_location")
 print('加盟数=', len(records))
 for record in records:
@@ -10,12 +12,11 @@ for record in records:
     print(record.a.string.strip(), end='\t')
     if 'http' in record.a['href']:
         print(record.a['href'], end='\t')
-    else:
-        print('\t', end='')
+
     # twitter情報があれば表示
     a_twitter = record.find('a', class_='social icon twitter')
     if a_twitter:
-        print(a_twitter['href'])
+        print(a_twitter['href'].replace('https://twitter.com/', '@'))
     else:
         # 改行のみ
         print('')
